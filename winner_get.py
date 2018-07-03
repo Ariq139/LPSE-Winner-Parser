d1d41bc8c6ed80355f63010154cae35b2e247343
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-07-03 12:03:43 +0700

    get name and type of the auction + proper getter

--- winner_get.py
+++ winner_get.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup as BS
 import urllib.request
-id = str(int(input('Auction ID: ')))
+import string
+
+id = str(input('Auction ID: '))
 html = urllib.request.urlopen("https://lpse.ugm.ac.id/eproc4/evaluasi/"+id+"/pemenang")
 #print(html.read())
 soup = BS(html, 'html.parser')
 #print(soup.prettify())
-print(soup.find_all('strong', limit=1))
+#print(soup.find_all('strong', limit=1))
+
+name = soup.find_all('td')[0]
+type = soup.find_all('td')[1]
+winner = soup.find_all('strong')[0]
+
+print(name.contents[0])
+print(type.contents[0])
+print(winner.contents[0])
