d5c875e3946333d65e248cc661eb581d3c981523
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-07-03 17:05:33 +0700

    test

---
+++ updatecheck.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup as BS
+import urllib.request
+
+max_code_as_of_development = 1055303 #fallback bila file terhapus
--- winner_get.py
+++ winner_get.py
@@ -1,18 +1,30 @@
 from bs4 import BeautifulSoup as BS
-import urllib.request
-import string
+import urllib.request, urllib.error
 
-id = str(input('Auction ID: '))
-html = urllib.request.urlopen("https://lpse.ugm.ac.id/eproc4/evaluasi/"+id+"/pemenang")
-#print(html.read())
-soup = BS(html, 'html.parser')
-#print(soup.prettify())
-#print(soup.find_all('strong', limit=1))
+id = int(input('Auction ID: '))
+str_id = str(id)
+#web = str(input('Site: '))
+#print("NOTE: Area Jogja")
+try:
+    html = urllib.request.urlopen("https://lpse.ugm.ac.id/eproc4/evaluasi/"+str_id+"/pemenang")
+    #print(html.read())
+    soup = BS(html, 'html.parser')
+    #print(soup.prettify())
+    #print(soup.find_all('strong', limit=1))
 
-name = soup.find_all('td')[0]
-type = soup.find_all('td')[1]
-winner = soup.find_all('strong')[0]
+except urllib.error.HTTPError as e:
+    print('The server couldn\'t fulfill the request.')
+    print('Error code: ', e.code)
+except urllib.error.URLError as e:
+    print('Failed to reach a server.')
+    print('Reason: ', e.reason)
+else:
+    name = soup.find_all('td')[0]
+    type = soup.find_all('td')[1]
+    winner = soup.find_all('td')[6]
+    [s.extract() for s in soup('strong')]
 
-print(name.contents[0])
-print(type.contents[0])
-print(winner.contents[0])
+    print(name.contents[0])
+    print(type.contents[0])
+    print(winner.contents[0])
+
