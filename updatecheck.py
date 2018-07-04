51f6591e5f8dde91f0806aeb32f585a15a324b8d
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-07-04 14:49:22 +0700

    update test script

--- updatecheck.py
+++ updatecheck.py
@@ -1,4 +1,26 @@
 from bs4 import BeautifulSoup as BS
-import urllib.request
+import urllib.request, urllib.error, sys
 
-max_code_as_of_development = 1055303 #fallback bila file terhapus
+def getURL(loc, auct_id):
+    html = urllib.request.urlopen("http://lpse."+loc+".id/eproc4/lelang/"+auct_id+"/pengumumanlelang")
+    soup = BS(html, 'html.parser')
+    
+    return soup
+
+def getAuct(i):
+    print("Checking LPSE UGM....")
+    try:
+        temp = getURL("ugm.ac", str(i)+"303")
+        if temp.find_all('td')[4].get_text() == "Lelang Sudah Selesai":
+            #getAuct(i+1)
+            print("check")
+        #if temp.find_all('title')[0].contents[0] == "Error":
+            #print("Total lelang yang sudah selesai: "+str(i)+"\n")
+        #elif temp.find_all('title')[0].contents[0] == "Akses Ditolak":
+            #getAuct(i+1)
+            #print("skip")
+    except (urllib.error.HTTPError, urllib.error.URLError, TimeoutError) as e:
+        print("connect error")
+if __name__ == "__main__":
+    i = 1
+    getAuct(i)
--- winner_get.py
+++ winner_get.py
@@ -1,30 +1,61 @@
 from bs4 import BeautifulSoup as BS
-import urllib.request, urllib.error
+import urllib.request, urllib.error, sys
 
-id = int(input('Auction ID: '))
-str_id = str(id)
-#web = str(input('Site: '))
-#print("NOTE: Area Jogja")
-try:
-    html = urllib.request.urlopen("https://lpse.ugm.ac.id/eproc4/evaluasi/"+str_id+"/pemenang")
-    #print(html.read())
-    soup = BS(html, 'html.parser')
-    #print(soup.prettify())
-    #print(soup.find_all('strong', limit=1))
+if __name__ == "__main__":
+    auct_id = int(sys.argv[1])
+    #loc = str(sys.argv[2])
+    loc = ""
+    loc_localize = ""
+    
+    str_id = str(auct_id)
+    if str_id.endswith("303"):
+        loc += "ugm.ac"
+        loc_localize += "LPSE UGM"
+    elif str_id.endswith("367"):
+        loc += "kulonprogokab.go"
+        loc_localize += "LPSE Kulon Progo"
+    elif str_id.endswith("021"):
+        loc += "jogjakota.go"
+        loc_localize += "LPSE Yogyakarta Kota"
+    elif str_id.endswith("054"):
+        loc += "slemankab.go"
+        loc_localize += "LPSE Sleman"
+    elif str_id.endswith("013"):
+        loc += "jogjaprov.go"
+        loc_localize += "LPSE Provinsi Yogyakarta"
+    elif str_id.endswith("621"):
+        loc += "gunungkidulkab.go"
+        loc_localize += "LPSE Gunungkidul"
+    elif str_id.endswith("285"):
+        loc += "bantulkab.go"
+        loc_localize += "LPSE Bantul"
+    
+    #web = str(input('Site: '))
+    #print("NOTE: Area Jogja")
+    try:
+        html = urllib.request.urlopen("http://lpse."+loc+".id/eproc4/evaluasi/"+str_id+"/pemenang")
+        #print(html.read())
+        soup = BS(html, 'html.parser')
+        #print(soup.prettify())
+        #print(soup.find_all('strong', limit=1))
 
-except urllib.error.HTTPError as e:
-    print('The server couldn\'t fulfill the request.')
-    print('Error code: ', e.code)
-except urllib.error.URLError as e:
-    print('Failed to reach a server.')
-    print('Reason: ', e.reason)
-else:
-    name = soup.find_all('td')[0]
-    type = soup.find_all('td')[1]
-    winner = soup.find_all('td')[6]
-    [s.extract() for s in soup('strong')]
-
-    print(name.contents[0])
-    print(type.contents[0])
-    print(winner.contents[0])
-
+    except urllib.error.HTTPError as e:
+        print("The server couldn't fulfill the request.")
+        print('Error code: ', e.code)
+        
+    except urllib.error.URLError as e:
+        print('Failed to reach a server.')
+        print('Reason: ', e.reason)
+    else:
+        try:
+            name = soup.find_all('td')[0]
+            type = soup.find_all('td')[1]
+            winner = soup.find_all('td')[6]
+        
+            print("Lokasi:", loc_localize, "\n")
+            print(name.contents[0])
+            print(type.contents[0])
+            print(winner.get_text())
+        except IndexError:
+            print("Lokasi:", loc_localize, "\n")
+            print("Pemenang tidak ditemukan")
