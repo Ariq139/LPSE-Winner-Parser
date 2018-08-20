52fd31e14f85c9fc8450977d460654f2e5c2ef8a
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-08-20 09:30:28 +0700

    change build to release

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LPSE_UGM_Winner_Parser.Properties {
+namespace LPSE_Winner_Parser.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LPSE_UGM_Winner_Parser.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LPSE_UGM_Winner_Parser.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LPSE_Winner_Parser.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
